0eb71791c5a52daba6757543ac14a4fec9bc5ceb
Otabek8866 <sobirovotabek1995@gmail.com>
2022-04-25 14:01:27 +0200

    Log parser changed

--- Log-Analysis/packet_sts.py
+++ Log-Analysis/packet_sts.py
@@ -1,24 +1,17 @@
-import sys
-INPUT_FILE = sys.argv[1]
+INPUT_FILE = "loglistener.txt"
 
 delays = {}
 pkt_sent = {}
 pkt_received = {}
-covergence_time_list = []
 
 
 def main():
-    total_sent_packets = 0
     with open(INPUT_FILE, "r") as f:
         for line in f:
             if not (("INFO: App" in line) and ("Sent_to" in line or "Received_from" in line)):
                 continue
             if "Sent_to" in line:
                 fields = line.split()
-
-                if fields[-1] == '1':
-                    covergence_time_list.append(fields)
-
                 node_id = int(fields[1][3:])
                 if node_id in pkt_sent:
                     pkt_sent[node_id][int(fields[-1])] = float(fields[0])
@@ -37,7 +30,6 @@ def main():
                 print("Error: line not recognized")
 
     for node_id in pkt_sent.keys():
-        total_sent_packets += len(pkt_sent[node_id])
         if node_id in pkt_received:
             pkt_id_list = sorted(set(pkt_sent[node_id].keys()) & set(
                 pkt_received[node_id].keys()))
@@ -62,25 +54,21 @@ def main():
         average_pdr += delays[i][2]
     average_pdr = average_pdr/len(delays.keys())
 
-    covergence_time_list.sort(reverse=True, key=lambda x: int(x[0]))
-
     print("\n*********** Statistics for Network *************")
     print("Average Delay: {:.2f} ms".format(total_time/counter))
     print("Packet Delivery Ratio: {:.2f}%".format(average_pdr))
-    print("Total Sent Packets: {}".format(total_sent_packets))
-    print("Network Convergence: {:.2f} minutes".format(float(
-        int(covergence_time_list[0][0])/1000)/60))
 
     print("\n********* Average Delays for each Node *********")
     for i in sorted(delays.keys()):
         if len(delays[i][0]) != 0:
-            print("Node {}: {:.2f} ms".format(
-                i, sum(delays[i][0])/len(delays[i][0])))
+            print("Node {}: {:.2f} ms".format(i, sum(delays[i][0])/len(delays[i][0])))
         else:
             print("Node {}: No packet received -> No Delay".format(i))
         print("Packet IDs not received: {}".format(delays[i][1]))
         print("Packet Delivery Ratio: {:.2f}%".format(delays[i][2]))
         print('-' * 50)
+    print("Number of Active Nodes:", len(pkt_sent))
+    print("Active nodes:", sorted(pkt_sent.keys()))
 
 
 if __name__ == "__main__":
